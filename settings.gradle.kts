96c7144fc48453e4a0e947607f80d6e12bce7fca
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2022-06-17 22:36:37 +0300

    gradle properties kotlin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "tankobon-server-kotlin"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        id("com.github.johnrengelman.shadow") version "7.1.2"
+    }
+}
