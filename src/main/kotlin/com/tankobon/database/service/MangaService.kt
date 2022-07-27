f660fda3b22b75ff83748308eaa5abc1f7300aaa
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2022-07-27 23:36:09 +0300

    add already proceeded manga to db

--- src/main/kotlin/com/tankobon/database/service/MangaService.kt
+++ src/main/kotlin/com/tankobon/database/service/MangaService.kt
@@ -1,6 +1,5 @@
 package com.tankobon.database.service
 
-import com.tankobon.database.DatabaseFactory
 import com.tankobon.database.DatabaseInstance
 import com.tankobon.database.model.Manga
 import com.tankobon.database.model.MangaModel
@@ -9,17 +8,17 @@ import com.tankobon.database.model.toManga
 import com.tankobon.globalThumbPath
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.update
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import java.io.File
 import java.util.UUID
 
-class MangaService() {
-    val database = DatabaseInstance.instance;
+class MangaService {
+    val database = DatabaseInstance.instance
 
     suspend fun getMangaList(): List<Manga> = newSuspendedTransaction(db = database) {
         return@newSuspendedTransaction MangaModel.selectAll().map { it.toManga() }
@@ -45,7 +44,7 @@ class MangaService() {
             }
 
         listMangaUpdate.forEach { e ->
-            if (e.title?.isNotEmpty() == true) {
+            if (MangaModel.select { MangaModel.id eq UUID.fromString(e.id) }.firstOrNull() == null) {
                 addMangaList(e)
             } else {
                 MangaModel.update({ MangaModel.id eq UUID.fromString(e.id) }) {
