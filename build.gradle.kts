562103bdb227adad31f1caab6953fdd72f7c0f35
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2022-08-23 22:42:17 +0300

    gradle rework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-val ktorVersion: String by project
-val logbackVersion: String by project
-val exposedVersion: String by project
-val h2Version: String by project
-val kotlinVersion: String by project
-val ktlint: Configuration by configurations.creating
-
 group = "com.tankobon"
 version = "0.0.1"
 description = "Server for Tankōbon - a flutter manga reader app. WIP."
@@ -14,10 +7,10 @@ application {
 
 plugins {
     application
-    kotlin("jvm")
-    kotlin("plugin.serialization")
-    id("com.github.johnrengelman.shadow")
-    id("io.gitlab.arturbosch.detekt")
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
 }
 
 repositories {
@@ -25,23 +18,23 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
-    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:2.1.0")
+    implementation("io.ktor:ktor-server-host-common-jvm:2.1.0")
+    implementation("io.ktor:ktor-server-netty-jvm:2.1.0")
+    implementation("io.ktor:ktor-server-auth-jvm:2.1.0")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.1.0")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.1.0")
+    implementation("io.ktor:ktor-server-status-pages:2.1.0")
+    implementation("io.ktor:ktor-server-call-logging:2.1.0")
+    implementation("io.ktor:ktor-server-content-negotiation:2.0.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
     implementation("org.mindrot:jbcrypt:0.4")
 
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-core:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-dao:0.39.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.39.2")
 
-    implementation("com.h2database:h2:$h2Version")
+    implementation("com.h2database:h2:2.1.214")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
@@ -51,9 +44,9 @@ dependencies {
 
     implementation("com.sksamuel.scrimage:scrimage-core:4.0.31")
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.10")
 }
 
 val outputDir = "${project.buildDir}/reports/ktlint/"
