73e5e2e54f80be7bf32999d43ccca5978d379a0c
Ilya Dobryakov <aspirin@govno.tech>
2022-08-23 22:42:17 +0300

    gradle rework

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,1 @@
 rootProject.name = "tankobon-server-kotlin"
-
-pluginManagement {
-    val kotlinVersion: String by settings
-    val shadowVersion: String by settings
-    val detektVersion: String by settings
-
-    plugins {
-        kotlin("jvm") version kotlinVersion
-        kotlin("plugin.serialization") version kotlinVersion
-        id("com.github.johnrengelman.shadow") version shadowVersion
-        id("io.gitlab.arturbosch.detekt") version detektVersion
-    }
-}
