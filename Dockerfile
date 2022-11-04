fd60b6c723059b64d2f3d98b512f5887db43e474
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2022-11-04 18:54:36 +0300

    move from java archive libs to native shell commands

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle shadowJar --no-daemon
 
-FROM eclipse-temurin:19-jre AS final
+FROM eclipse-temurin:17-jre AS final
+RUN apt update && apt -y install unrar unzip
 RUN mkdir /opt/app
 WORKDIR /opt/app
 COPY --from=build /home/gradle/src/build/libs/tankobon-*-all.jar /opt/app/tankobon.jar
--- src/main/kotlin/com/tankobon/manga/archive/UnZIP.kt
+++ src/main/kotlin/com/tankobon/manga/archive/UnZIP.kt
@@ -1,11 +1,11 @@
 package com.tankobon.manga.archive
 
-import net.lingala.zip4j.ZipFile
+import com.lordcodes.turtle.shellRun
 import java.io.File
 
 fun unZIP(path: File) {
     val newPath = File("${path.parent}/${path.nameWithoutExtension}/0")
     newPath.mkdirs()
-    ZipFile(path.path).extractAll(newPath.path)
+    shellRun("unzip", listOf(path.path, "-d", newPath.path))
     path.delete()
 }
