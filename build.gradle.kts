0d8f6eb84295445ce6126113b49f38b5ae503445
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2023-02-05 23:28:07 +0300

    gradle rework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+val kotlinVersion: String by project
+val ktorVersion: String by project
+val detektVersion: String by project
+
 tasks.wrapper {
     gradleVersion = "7.6"
     distributionType = Wrapper.DistributionType.BIN
@@ -12,10 +16,10 @@ application {
 
 plugins {
     application
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("com.github.johnrengelman.shadow")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 repositories {
@@ -23,26 +27,26 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-host-common-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-netty-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-auth-jvm:2.1.3")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-status-pages:2.1.3")
-    implementation("io.ktor:ktor-server-call-logging:2.1.3")
-    implementation("io.ktor:ktor-server-content-negotiation:2.1.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("org.mindrot:jbcrypt:0.4")
 
-    implementation("org.jetbrains.exposed:exposed-core:0.40.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
 
     implementation("com.h2database:h2:2.1.214")
     implementation("com.zaxxer:HikariCP:5.0.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
     implementation("com.lordcodes.turtle:turtle:0.8.0")
 
@@ -52,12 +56,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.4")
     implementation("org.slf4j:slf4j-api:2.0.3")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.21")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
 }
 
-val outputDir = "${project.buildDir}/reports/ktlint/"
-val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "tankobon-server-kotlin"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    val detektVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        id("com.github.johnrengelman.shadow") version "7.1.2"
+        id("io.gitlab.arturbosch.detekt") version detektVersion
+    }
+}
